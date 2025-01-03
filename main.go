3dc6186d599451ee53703dee340d6f03e21e79c1
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-01-04 01:10:48 +0530

    (ADD): new download snippet

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-//	"fmt"
-//	"os"
-	entrypoint "github.com/serene-brew/ztorrent/interfaces"
-//	bencode "github.com/serene-brew/ztorrent/bencode"
-//	crawler "github.com/serene-brew/ztorrent/crawler"
-  //  mag "github.com/serene-brew/ztorrent/torrent"
+	"fmt"
+	"os"
+	"strings"
+	// entrypoint "github.com/serene-brew/ztorrent/interfaces"
+	bencode "github.com/serene-brew/ztorrent/bencode"
+	crawler "github.com/serene-brew/ztorrent/crawler"
+   	mag "github.com/serene-brew/ztorrent/torrent"
 )
 
 func main() {
 	//-----------------------------------------------------------------------------
 	//torrent file parsing and peers extraction
-/*	torrent, err := bencode.ParseTorrentFile("example.torrent")
+	torrent, err := bencode.ParseTorrentFile("example.torrent")
 	if err != nil {
 		fmt.Println("error reading the torrent file")
 		os.Exit(1)
@@ -30,7 +31,7 @@ func main() {
     }
 	//-----------------------------------------------------------------------------
     // crawler test script*/ 
-/*	query := "Assassins Creed unity"
+	query := "terminator 1"
 	data, err := crawler.GetInfoMediaQuery(query)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -41,12 +42,12 @@ func main() {
 		for _, terms := range record{
 			fmt.Println(terms)
 		}
-	}*/
-	//magnet := crawler.GetMagnet(data[0][2].(string), data[0][1].(string))
+	}
+	magnet := crawler.GetMagnet(data[0][2].(string), data[0][1].(string))
 
 	//-----------------------------------------------------------------------------
     // crawler magnet URI parsing and peers extraction 
-/*	magnetLink := magnet
+	magnetLink := magnet
     metadata, err := bencode.ParseMagnetLink(magnetLink)
     if err != nil {
         fmt.Println("Error parsing magnet link:", err)
@@ -63,10 +64,35 @@ func main() {
         os.Exit(1)
     }
 
-    fmt.Println(magnet)*/
+	// Ask user for file selection
+    fmt.Println("\nEnter file extensions to download (comma-separated, e.g., mp4,mkv):")
+    var extInput string
+    fmt.Scanln(&extInput)
+    
+    extensions := []string{}
+    if extInput != "" {
+        extensions = strings.Split(extInput, ",")
+    }
+
+    fmt.Println("Enter maximum file size in MB (0 for no limit):")
+    var maxSizeMB int64
+    fmt.Scanln(&maxSizeMB)
+
+	selection := mag.FileSelection{
+        Extensions: extensions,
+		MaxSize:    maxSizeMB * 1024 * 1024,
+    }
+
+    err = mag.DownloadSelectedFilesFromMagnet(magnet, selection)
+    if err != nil {
+        fmt.Println("Error downloading files:", err)
+        os.Exit(1)
+    }
+
+    fmt.Println(magnet)
 
 	//-----------------------------------------------------------------------------
-	entrypoint.Entrypoint()	
+	// entrypoint.Entrypoint()	
     
 }
 
