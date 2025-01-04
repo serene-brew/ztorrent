4cd735983b8c8f7c3cae1aa0d85be4420bfcadbc
Imon Chakraborty <chakraborty2005imon@gmail.com>
2025-01-05 01:18:59 +0530

    (ADD): structures for returning values

--- torrent/structs.go
+++ torrent/structs.go
@@ -20,9 +20,44 @@ func DefaultConfig() Config {
     }
 }
 
-type FileSelection struct {
-    Extensions []string
-    MaxSize    int64
-    MinSize    int64
-    Paths      []string
+// FileInfo holds information about a single file
+type FileInfo struct {
+    Name     string
+    Size     int64
+    Type     string
+    Path     string
+    Complete bool
+}
+
+// TorrentInfo holds comprehensive torrent information
+type TorrentInfo struct {
+    Name       string
+    InfoHash   string
+    TotalSize  int64
+    Files      []FileInfo
+    FilesByExt map[string][]FileInfo
+}
+
+// PeerInfo holds peer connection information
+type PeerInfo struct {
+    Address  string
+    Active   bool
+    Stats    PeerStats
+}
+
+// PeerStats holds peer statistics
+type PeerStats struct {
+    TotalPeers   int
+    ActivePeers  int
+    PendingPeers int
+}
+
+// ProgressInfo holds download progress information
+type ProgressInfo struct {
+    Completed   int64
+    Total       int64
+    Percentage  float64
+    Speed       float64
+    TimeElapsed float64
+    ETA         float64
 }
